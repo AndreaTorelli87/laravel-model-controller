69d017b0911825aeb3ae5c4f3e4be5d1412adbb0
Andrea Torelli <gioran326@protonmail.com>
2023-05-11 14:54:17 +0200

    Base finito!

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,36 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $movies = Movie::all();
+        return view("home", compact("movies"));
+    }
 }
+
+
+
+// namespace App\Http\Controllers\Guest;
+
+// use App\Http\Controllers\Controller;
+// use App\Models\Book;
+// use Illuminate\Http\Request;
+
+// class PageController extends Controller
+// {
+//     public function index() {
+
+//         $newBook = new Book();
+
+//         $newBook->id = 5;
+//         $newBook->title = 'ciao mondo';
+//         $newBook->vote = 4;
+//         $newBook->save();
+
+//         $books = Book::where('vote', '>=', 4)->get();
+//         return view('home', compact('books'));
+
+//     }
